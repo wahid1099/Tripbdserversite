51130ae8aa921e4d44c5639567abc56ff5c912a1
Abdul Wahid <wahdiahmed890@gmail.com>
2021-11-01 22:36:14 +0600

    removing cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const {MongoClient}=require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
-const cors=require('cors');
+// const cors=require('cors');
 const app=express();
 const port=process.env.PORT || 5000;
 
@@ -14,7 +14,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 //middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 
